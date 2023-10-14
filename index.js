896fa1776238bd303cfc60e0c51f50beefab357a
dante4rt <rxmxdhxni@gmail.com>
2023-10-14 19:52:25 +0700

    feat: add all scripts in one commit

---
+++ index.js
@@ -0,0 +1,57 @@
+import chalk from 'chalk';
+import { Wallet, ethers } from 'ethers';
+import { appendFileSync } from 'fs';
+import moment from 'moment';
+import readlineSync from 'readline-sync';
+
+function createAccountETH() {
+  const wallet = ethers.Wallet.createRandom();
+  const privateKey = wallet.privateKey;
+  const publicKey = wallet.publicKey;
+
+  return { privateKey, publicKey };
+}
+
+(async () => {
+  try {
+    const totalWallet = readlineSync.question(
+      chalk.yellow('Input how much the wallet you want: ')
+    );
+
+    let count = 1;
+
+    if (totalWallet > 1) {
+      count = totalWallet;
+    }
+
+    while (count > 0) {
+      const createWalletResult = createAccountETH();
+      const theWallet = new Wallet(createWalletResult.privateKey);
+
+      if (theWallet) {
+        appendFileSync(
+          './result.txt',
+          `${theWallet.address} | ${createWalletResult.privateKey}\n`
+        );
+        console.log(
+          chalk.green(
+            `[${moment().format('HH:mm:ss')}] => ` + 'Wallet created...!'
+          )
+        );
+      }
+
+      count--;
+    }
+    // change the timer (3000) if you think this is too long, in miliseconds.
+    setTimeout(() => {
+      console.log(
+        chalk.green(
+          'All wallets have been created. Check result.txt to check your results.'
+        )
+      );
+    }, 3000);
+    return;
+  } catch (error) {
+    console.log(chalk.red('Your program was error! Message: ' + error));
+  }
+})();

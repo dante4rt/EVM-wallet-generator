ecc8b1209c125ae5a17f5db70103b69a40d0899b
dante4rt <rxmxdhxni@gmail.com>
2023-12-25 00:18:13 +0800

    refactor: add commands on main func

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { appendFileSync } from 'fs';
 import moment from 'moment';
 import readlineSync from 'readline-sync';
 
+// Function to create a new Ethereum account
 function createAccountETH() {
   const wallet = ethers.Wallet.createRandom();
   const privateKey = wallet.privateKey;
@@ -13,27 +14,33 @@ function createAccountETH() {
   return { privateKey, publicKey, mnemonicKey };
 }
 
+// Main function using async IIFE (Immediately Invoked Function Expression)
 (async () => {
   try {
+    // Get the total number of wallets to create from user input
     const totalWallet = readlineSync.question(
       chalk.yellow('Input how much the wallet you want: ')
     );
 
     let count = 1;
 
+    // If the user entered a valid number greater than 1, set the count
     if (totalWallet > 1) {
       count = totalWallet;
     }
 
+    // Create the specified number of wallets
     while (count > 0) {
       const createWalletResult = createAccountETH();
       const theWallet = new Wallet(createWalletResult.privateKey);
 
       if (theWallet) {
+        // Append wallet details to result.txt
         appendFileSync(
           './result.txt',
           `Address: ${theWallet.address} | Private Key: ${createWalletResult.privateKey} | Mnemonic: ${createWalletResult.mnemonicKey}\n`
         );
+        // Display success message with the wallet address and timestamp
         console.log(
           chalk.green(
             `[${moment().format('HH:mm:ss')}] => ` + 'Wallet created...! Your address: ' + theWallet.address
@@ -43,16 +50,19 @@ function createAccountETH() {
 
       count--;
     }
-    // change the timer (3000) if you think this is too long, in miliseconds.
+
+    // Display final message after creating all wallets
+    // Change the timer (3000) if you think this is too long, in milliseconds.
     setTimeout(() => {
       console.log(
         chalk.green(
-          'All wallets have been created. Check result.txt to check your results (the address, mnemonic and private key).'
+          'All wallets have been created. Check result.txt to check your results (the address, mnemonic, and private key).'
         )
       );
     }, 3000);
     return;
   } catch (error) {
-    console.log(chalk.red('Your program was error! Message: ' + error));
+    // Display error message if an error occurs
+    console.log(chalk.red('Your program encountered an error! Message: ' + error));
   }
 })();
